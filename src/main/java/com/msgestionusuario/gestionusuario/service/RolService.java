24f03d82a9fbe85ccdec883117502c5432b2e2d8
BenjaMorenoo <benjaben100@gmail.com>
2025-05-28 21:36:15 -0400

    first commit

---
+++ src/main/java/com/msgestionusuario/gestionusuario/service/RolService.java
@@ -0,0 +1,66 @@
+package com.msgestionusuario.gestionusuario.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.msgestionusuario.gestionusuario.model.Rol;
+import com.msgestionusuario.gestionusuario.model.Roles;
+import com.msgestionusuario.gestionusuario.repository.RolRepository;
+
+@Service
+public class RolService {
+    
+    @Autowired
+    private RolRepository rolRepository;
+
+    public Rol crearRol(Rol rol) {
+        return rolRepository.save(rol);
+    }
+
+   /* public Rol crearRolxNombreRol(Rol rol){
+        rol.setNombreRol(Roles.valueOf(rol.getNombreRol().toString()));
+        return rolRepository.save(rol);
+    }*/
+
+    public Rol findxNombreRol(String nombreRol) {
+        return rolRepository.findByNombreRol(Roles.valueOf(nombreRol));
+    }
+
+    public Rol findxIdRol(int idRol) {
+        return rolRepository.findById(idRol);
+    }
+
+
+    
+    
+
+    public List<Rol> findAllRoles() {
+        return rolRepository.findAll();
+    }
+
+    public Rol editRol(Integer idRol, Rol rol) {
+        Optional<Rol> rolExistente = rolRepository.findById(idRol);
+        if (rolExistente.isPresent()) {
+            Rol rolActualizado = rolExistente.get();
+            rolActualizado.setNombreRol(rol.getNombreRol());
+            rolActualizado.setFuncion(rol.getFuncion());
+            return rolRepository.save(rolActualizado);
+        }
+        return null;
+    }
+
+    public Rol eliminarRol(int idRol) {
+        Rol rol = rolRepository.findById(idRol);
+        if (rol != null && (rol.getUsuarios() == null || rol.getUsuarios().isEmpty())) {
+            rolRepository.deleteById(idRol);
+            return rol;
+        }
+        // si tengo usuarios asociados al rol, no lo elimino
+        return null;
+    }
+
+}
+
